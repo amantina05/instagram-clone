f4805aef277ed5b48359e883222dfee1686b73ea
Amantina Sosa <amantinasosa@gmail.com>
2020-09-23 11:12:38 -0400

    after deployment additions

---
+++ build/precache-manifest.2ac17ec221e0a154d30f4b7f04837058.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3125373a4e8c9e07960ac8dc9bcbe635",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a47d727363231af343da",
+    "url": "/static/css/main.45ced9c7.chunk.css"
+  },
+  {
+    "revision": "58bdb96c7efe8d62f6f5",
+    "url": "/static/js/2.ae2fb13d.chunk.js"
+  },
+  {
+    "revision": "e154812a13cd8fb7678b353f8e3f53cd",
+    "url": "/static/js/2.ae2fb13d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a47d727363231af343da",
+    "url": "/static/js/main.bb380648.chunk.js"
+  },
+  {
+    "revision": "12ffa9c2a2e2bca05bc1",
+    "url": "/static/js/runtime-main.a41b20c0.js"
+  }
+]);
